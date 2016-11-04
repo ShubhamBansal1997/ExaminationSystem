85331fbf4f300ecff3c0f21dc8c621c11c1f7956
ShubhamBansal1997 <shubhambansal17@hotmail.com>
2016-11-04 18:57:51 +0530

    changes

--- resources/views/pages/chap_details.blade.php
+++ resources/views/pages/chap_details.blade.php
@@ -23,7 +23,7 @@
                       <div class="dd nestable">
                         <ol class="dd-list">
                           <li class="dd-item" data-id="1">
-                            <a target="_blank" href="#"><div class="dd3-content">All Questions (0)</div>
+                            <a target="_blank" href="{{URL::to('qpage')}}/{{ $sub_id }}/{{ $chap_id }}/allques"><div class="dd3-content">All Questions ({{ \App\Questions::a_ques($chap_id) }})</div>
                             </a>
                             <div class="progress progress-striped active">
                               <div class="progress-bar progress-bar-default" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
@@ -32,31 +32,31 @@
                             </div>  
                             <ol class="dd-list">
                               <li class="dd-item"  data-id="2">
-                                <a target="_blank" href="#">
-                                  <div class="dd3-content">Easy (0)</div>
+                                <a target="_blank" href="{{URL::to('qpage')}}/{{ $sub_id }}/{{ $chap_id }}/easy">
+                                  <div class="dd3-content">Easy ({{ \App\Questions::e_ques($chap_id) }})</div>
                                 </a>
                                 <div class="progress progress-striped active">
-                                  <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+                                  <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: {{ \App\Questions::e_quesp($chap_id) }}%">
                                   <span class="sr-only">100% Complete (success)</span>
                                   </div>
                                 </div>
                               </li>
                               <li class="dd-item"   data-id="3">
-                                <a target="_blank" href="#"> 
-                                  <div class="dd3-content">Medium (0)</div>
+                                <a target="_blank" href="{{URL::to('qpage')}}/{{ $sub_id }}/{{ $chap_id }}/medium"> 
+                                  <div class="dd3-content">Medium ({{ \App\Questions::m_ques($chap_id) }})</div>
                                 </a>
                                 <div class="progress progress-striped active">
-                                  <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 90%">
+                                  <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: {{ \App\Questions::m_quesp($chap_id) }}%">
                                   <span class="sr-only">100% Complete (success)</span>
                                   </div>
                                 </div>
                               </li>
                               <li class="dd-item"  data-id="4">
-                                <a href= target="_blank" href="#">  
-                                  <div class="dd3-content">Difficult (0)</div> 
+                                <a href= target="_blank" href="{{ URL::to('qpage')}}/{{ $sub_id }}/{{ $chap_id }}/difficult">  
+                                  <div class="dd3-content">Difficult ({{ \App\Questions::d_ques($chap_id) }})</div> 
                                 </a>
                                 <div class="progress progress-striped active">
-                                  <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 56%">
+                                  <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: {{ \App\Questions::d_quesp($chap_id) }}%">
                                   <span class="sr-only">100% Complete (success)</span>
                                   </div>
                                 </div>
@@ -64,11 +64,11 @@
                                 
                             </ol>
                             <li class="dd-item dd3-item" data-id="5">
-                              <a target="_blank" href="#">
-                                <div class="dd3-content">Important Questions (0)</div>
+                              <a target="_blank" href="{{URL::to('qpage')}}/{{ $sub_id }}/{{ $chap_id }}/imp">
+                                <div class="dd3-content">Important Questions ({{ \App\Questions::i_ques($chap_id) }})</div>
                               </a>
                               <div class="progress progress-striped active">
-                                <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 90%">
+                                <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: {{ \App\Questions::i_quesp($chap_id) }}%">
                                   <span class="sr-only">60% Complete (warning)</span>
                                 </div>
                               </div>
