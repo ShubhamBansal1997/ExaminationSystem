5bd761ac1e22129f44da9bb55220051f5d7ff022
ShubhamBansal1997 <shubhambansal17@hotmail.com>
2017-01-30 00:50:22 +0530

    frequent changes

--- resources/views/pages/chap_name.blade.php
+++ resources/views/pages/chap_name.blade.php
@@ -42,13 +42,13 @@
                 
                 <div class="collection waves-color-demo">
                   @foreach(\App\Chapters::where('sub_id',$sub_id)->where('std',$std)->get() as $i=>$chapters)
-                  @if($i==0)
+                  
                   <div class="collection-item">{{ str_limit($chapters->chap_name, $limit = 41, $end = '...') }}<a href="{{ URL::to('home') }}/{{ $sub_id }}/{{ $std }}/{{ $chapters->chap_id }}" class="waves-effect waves-light btn secondary-content" style="margin-right:7px;margin-top: 10px;">Start</a>
                   </div>
-                  @else
-                  <div class="collection-item">{{ str_limit($chapters->chap_name, $limit = 41, $end = '...') }}<a href="#" class="waves-effect btn secondary-content purple darken-4" style="margin-top: 10px;">Unlock</a>
-                  </div>
-                  @endif
+                  
+                  <!-- <div class="collection-item">{{ str_limit($chapters->chap_name, $limit = 41, $end = '...') }}<a href="#" class="waves-effect btn secondary-content purple darken-4" style="margin-top: 10px;">Unlock</a>
+                  </div> -->
+                  
                   @endforeach
                   
                 </div>
