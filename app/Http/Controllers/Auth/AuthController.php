a4393905db853a6910ee106e0dedfddcd0bb5c1a
Shubham Bansal <shubhambansal17@hotmail.com>
2017-06-07 10:18:43 +0530

    updated the error in code

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -14,7 +14,7 @@ use App\User;
 
 class AuthController extends Controller
 {
-   use AuthenticatesAndRegistersUsers, ThrottlesLogins; 
+   use AuthenticatesAndRegistersUsers, ThrottlesLogins;
    public function __construct()
     {
         $this->redirectPath = route('home');
@@ -24,23 +24,22 @@ class AuthController extends Controller
     {
         return Socialite::driver($provider)->redirect();
     }
-    
+
      public function handleProviderCallback($provider)
     {
      //notice we are not doing any validation, you should do it
 
         $user = Socialite::driver($provider)->user();
-         
+
         // stroing data to our use table and logging them in
         $data = [
             'name' => $user->getName(),
             'email' => $user->getEmail()
         ];
-     
+
         Auth::login(User::firstOrCreate($data));
 
         //after login redirecting to home page
         return redirect($this->redirectPath());
     }
-}
 }
