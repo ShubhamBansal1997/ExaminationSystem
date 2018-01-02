9f054ff7c543dd2a5de91fd374db676fbfe11fa1
Shubham Bansal <shubhambansal17@hotmail.com>
2018-01-02 22:15:33 +0530

    updated the chap details page

--- app/Http/Controllers/UserDashboardController.php
+++ app/Http/Controllers/UserDashboardController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Session;
 use App\Http\Requests;
 use App\Chapters;
+use App\Questions;
+use App\Questions_attempt;
 use Illuminate\Support\Facades\Redirect;
 
 class UserDashboardController extends Controller
@@ -50,12 +52,55 @@ class UserDashboardController extends Controller
    	}
    	public function chap_page($sub_id,$std,$chap_id)
    	{
+		$email = Session::get('email');
    		$check = Chapters::where('sub_id',$sub_id)
    						->where('std',$std)
    						->where('chap_id',$chap_id)
-   						->count();
+						->count();
+		$allques = Questions::where('chap_id',$chap_id)
+							->count();
+		$easy = Questions::where('chap_id',$chap_id)
+							->where('ques_level',1)
+							->count();
+		$medium = Questions::where('chap_id',$chap_id)
+							->where('ques_level',2)
+							->count();
+		$diff = Questions::where('chap_id',$chap_id)
+							->where('ques_level',3)
+							->count();
+		$imp = Questions::where('chap_id',$chap_id)
+							->where('ques_imp',1)
+							->count();
+		$ar = Questions::where('chap_id',$chap_id)
+							->where('ques_ar', 1)
+							->count();
+		$attempt = Questions_attempt::where('chap_id',$chap_id)
+                          			->where('user_email',$email)
+									  ->count();
+									  
+		$unattempt = $allques - $attempt;
+		$correct = Questions_attempt::where('chap_id',$chap_id)
+									->where('user_email',$email)
+									->where('ques_status','correct')
+									->count();
+		$incorrect = Questions_attempt::where('chap_id',$chap_id)
+										->where('user_email',$email)
+										->where('ques_status','incorrect')
+										->count();
+		$data = [
+			'allques' =>  $allques,
+			'easy' => $easy,
+			'medium' => $medium,
+			'diff' => $diff,
+			'imp' => $imp,
+			'ar' => $ar,
+			'attempt' => $attempt,
+			'unattempt' => $unattempt,
+			'correct' => $correct,
+			'incorrect' => $incorrect
+		];
    		if($check)
-   			return view('pages.chap_details',compact('sub_id','class','chap_id'));
+   			return view('pages.chap_details',compact('data','chap_id','sub_id'));
    		else
    			return view('errors.503');
    	}
