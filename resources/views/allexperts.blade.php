bf735c5c237455f07bf676f39396535b953d7387
Shubham Bansal <shubhambansal17@hotmail.com>
2018-04-25 01:58:56 +0530

    update

--- resources/views/allexperts.blade.php
+++ resources/views/allexperts.blade.php
@@ -87,7 +87,7 @@
                   <div class="card-block">
                     <h4 class="grey-color">{{ $expert->first_name }} {{ $expert->last_name }}</h4>
                     {!! $expert->rank_in_various_exams !!}
-                    {{ $expert->quote }}
+                    <p class="mt-0 pt-0 font-9">{{ $expert->quote }}</p>
                     @if($expert->status!=1)
                     <p class="mt-0 pt-0 font-9"><b>Availability: </b>
                      Currently Unavailable
